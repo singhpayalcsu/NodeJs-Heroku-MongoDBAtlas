37b0a8573790990728246c461b06856ab0ea2268
Payal Singh <singhpayalsp@Payals-MacBook-Pro.local>
2021-03-11 21:25:25 -0800

    Add MONGODB_URI

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,38 @@
 var express = require('express');
 var router = express.Router();
 
+const { MongoClient } = require("mongodb");
+
+
+const uri = process.env.MONGODB_URI;
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router
+.get('/', function(req, res, next) {
   res.render('index', { title: 'MongoDB' });
-});
+})
+.get('/mongodb', async (req, res) => {
+  const client = new MongoClient(uri, { useUnifiedTopology: true });
+   
+  
+  try {
+    await client.connect();
+    await listRowOfDataBase(client, res);
+  
+  } catch(err) {
+    console.log(err);
+  }
+  finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+ });
 
 module.exports = router;
+
+async function listRowOfDataBase(client, res) {
+    const result = await client.db("myFirstDatabase").collection("myFirstDatabaseCollection").find().toArray();;
+
+    console.log(result);
+    res.render('mongodb', {"results": result});
+};
